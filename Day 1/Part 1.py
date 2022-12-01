2833637a14a4d461eedf49f5ce21252f9d6e0bfb
Andrew <32670729+mazalkov@users.noreply.github.com>
2022-12-01 18:54:25 +0000

    Create Part 1.py

---
+++ Day 1/Part 1.py
@@ -0,0 +1,16 @@
+with open("input.txt") as f:
+    lines = f.read().splitlines()
+
+
+sums = []
+running_total = 0
+for n in lines:
+    if n != "":
+        running_total += int(n)
+    else:
+        sums.append(running_total)
+        running_total = 0
+
+
+print(max(sums))
+# 68292
